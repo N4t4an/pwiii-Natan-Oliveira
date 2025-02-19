93951e10583e1554970964fcaf05cc4c9426607a
N4t4an <incrivelnatan@gmail.com>
2025-02-19 17:59:39 -0300

    Atividade PHP uhuuu

---
+++ exercicio1.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$val1 = 20;
+$val2 = 750;
+$val3 = 2;
+
+
+
+echo "$val1";
+echo "<br> $val2";
+echo "<br> $val3";
+
+$media = ($val1+$val2+$val3)/3;
+
+echo "<br> Média: ", $media;
+
+?>
